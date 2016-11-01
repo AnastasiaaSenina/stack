3533159c4b8f28a454c9d4a8c54d003aebcfe721
Anastasiaa Senina <miss.s-anas.itmm@yandex.ru>
2016-11-01 19:24:34 +0400

    implementation stack

---
+++ include/stack.h
@@ -0,0 +1,19 @@
+#include "conio.h"
+#include "stdio.h"
+struct CNode{
+	int data;
+	CNode* next;
+};
+class stack{
+	CNode *top;
+	int count;
+	int max;
+public:
+	stack(){ top = 0; max = 5; count = 0; }
+	bool push(int x);
+	int& GetTop();
+	bool pop();
+	int min();
+	int getcount(){ return count;}
+	void print();
+};
--- src/stack.cpp
+++ src/stack.cpp
@@ -1,1 +1,51 @@
 #include "stack.h"
+bool stack::push(int x){
+	if (count != max){
+		CNode *node = new CNode;
+		node->data = x;
+		node->next = top;
+		top = node;
+		count++;
+		return true;
+	}
+	else throw 1;
+}
+int& stack::GetTop(){
+	if (top != 0)
+		return top->data;
+	else throw 1;
+}
+bool stack::pop(){
+	if (top != 0){
+		CNode *s = top;
+		top = top->next;
+		delete s;
+		count--;
+		return true;
+	}
+	else throw 1;
+	}
+int stack::min(){
+	if (top != 0){
+		CNode* list = top->next;
+		int min = top->data;
+		while (list != 0){
+			if (min > list->data)
+				min = list->data;
+			list = list->next;
+		}
+		return min;
+	}
+	else throw 1;
+	}
+
+void stack::print(){
+	if (top != 0){
+		CNode* node = top;
+		while (node != 0){
+			printf("%d  ", node->data);
+			node = node->next;
+		}
+	}
+	else printf("stack is empty");
+}
