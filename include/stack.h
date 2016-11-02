2df44862e582c374ae6cbef5248b414a02dc6651
Anastasiaa Senina <miss.s-anas.itmm@yandex.ru>
2016-11-02 15:39:05 +0400

    +cpplint

--- include/stack.h
+++ include/stack.h
@@ -1,19 +1,23 @@
-#include "conio.h"
-#include "stdio.h"
-struct CNode{
-	int data;
-	CNode* next;
+#ifndef INCLUDE_STACK_H_
+#define INCLUDE_STACK_H_
+
+#include <conio.h>
+#include <stdio.h>
+struct CNode {
+  int data;
+  CNode* next;
 };
-class stack{
-	CNode *top;
-	int count;
-	int max;
-public:
-	stack(){ top = 0; max = 5; count = 0; }
-	bool push(int x);
-	int& GetTop();
-	bool pop();
-	int min();
-	int getcount(){ return count;}
-	void print();
+class stack {
+  CNode *top;
+  int count;
+  int max;
+ public:
+  stack() { top = 0; max = 5; count = 0; }
+  bool push(int x);
+  int& GetTop();
+  bool pop();
+  int min();
+  int getcount() { return count;}
+  void print();
 };
+#endif  // INCLUDE_STACK_H_
